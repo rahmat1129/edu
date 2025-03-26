86d14607357afa576514a77357600151d3c47abf
Md Rahmat Ali <167500738+rahmat1129@users.noreply.github.com>
2025-03-26 15:21:44 +0530

    Add student data

--- students.js
+++ students.js
@@ -1,2 +1,4 @@
 
 {"schoolId":"SCH1742981646799","classNumber":"5","rollNumber":"1","studentName":"Sag","photoURL":"https://gggg.com/hhh.png"}
+
+{"schoolId":"SCH1742982629874","classNumber":"1","rollNumber":"1","studentName":"Dfg","photoURL":"https://raw.githubusercontent.com/rahmat1129/edu/main/student_photos/student_1742982694878_Screenshot_20250325-140203.jpg"}
